1f9776786a41aaef22f1146a37985b57093923ce
Максим Кротков <stalker-2204@mail.ru>
2022-01-30 10:28:09 +0300

    upgrade

--- dicegame/dicegame.cpp
+++ dicegame/dicegame.cpp
@@ -1,19 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
 
-void dice_init(int arr[], int size) {
 
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 6 + 1;
-    }
-}
-void arr_dice_print(int arr[], int size) {
 
-    
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
+int dice_print() {
+
+    int n = rand() % 6 + 1;
+
+    cout << " _______" << endl;
+    cout << "/______/|" << endl;
+    cout << "|     | |" << endl;
+    cout << "|  " << n << "  | |" << endl;
+    cout << "|_____|/" << endl;
     cout << endl;
+    return n;
 }
 
 
@@ -32,32 +32,58 @@ int main()
     const int size = 5;
     int player_dice_1[size], player_dice_2[size], comp_dice_1[size], comp_dice_2[size];
     int summ_player = 0, summ_comp = 0, step;
-
-    dice_init(player_dice_1, size);
-    dice_init(player_dice_2, size);
-    dice_init(comp_dice_1, size);
-    dice_init(comp_dice_2, size);
-
-    arr_dice_print(player_dice_1, size);
-    arr_dice_print(player_dice_2, size);
-    arr_dice_print(comp_dice_1, size);
-    arr_dice_print(comp_dice_2, size);
-
+    
     step = rand() % 2;
     if (step == 1) {
         cout << "Игрок ходит первым" << endl;        
         system("pause");
 
+        for (int i = 0; i < size; i++) {
+            cout << "Ход игрока" << endl;
+            system("pause");
+            summ_player += dice_print();
+            //cout << summ_player << endl;
+            summ_player += dice_print();
+            //cout << summ_player << endl;
+
+            cout << "Ход компьютера" << endl;
+            summ_comp += dice_print();
+            //cout << summ_comp << endl;
+            summ_comp += dice_print();
+            //cout << summ_comp << endl;
+            
+        }
 
     }
     else {
         cout << "Компьютер ходит первым" << endl;
         
+        for (int i = 0; i < size; i++) {
+            cout << "Ход компьютера" << endl;
+            summ_comp += dice_print();
+            //cout << summ_comp << endl;
+            summ_comp += dice_print();
+            //cout << summ_comp << endl;
+
+            cout << "Ход игрока" << endl;
+            system("pause");
+            summ_player += dice_print();
+            //cout << summ_player << endl;
+            summ_player += dice_print();
+            //cout << summ_player << endl;
+        }
         
     }
 
+    if (summ_player > summ_comp) {
+        cout << "Победил игрок" << endl;
+    }
+    else {
+        cout << "Победил компьютер" << endl;
+    }
+    float sr = summ_player / (size * 2), sr2 = summ_comp / (size * 2);
 
-
-
+    cout << "Среднее значение игрока " << sr << endl;
+    cout << "Среднее значение для компьютера " << sr2 << endl;
 
 }
